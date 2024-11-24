a33b6a39acd395e110e61ae3c93676c437d826d8
Marlon <marlondesouzajlle@hotmail.com>
2024-11-24 20:28:45 -0300

    docs: ReadME redesigned for better understanding

--- README.md
+++ README.md
@@ -16,66 +16,54 @@
 
 - **Aprendizado Contínuo ou Online (se possível):** Melhorar o desempenho com dados acumulados para tornar a recomendação mais precisa ao longo do tempo.
 
-## Colunas Necessárias:
+## Estrutura dos Dados
 
-- distance:
-    - Indica a distancia capturada pelo sensor (abaixo de 40 cm pode indicar que o carro esta presente na vaga)
+### **Colunas Geradas**
 
-- light:
-    - Indica a luz capturada pelo sensor, nesse caso, valores mais elevados significam ambientes mais escuros e valores mais baixos (+ próximos de 0) indicam ambientes iluminados.
+| Coluna             | Descrição                                                                 |
+|---------------------|---------------------------------------------------------------------------|
+| `Vaga`             | Identificação única da vaga (ex.: `A1`, `B5`).                           |
+| `Status`           | Indica se a vaga está ocupada (`0`) ou livre (`1`).                      |
+| `Distance (cm)`    | Distância medida pelo sensor:                                             |
+|                    |  ≤ 40: Vaga ocupada                                                     |
+|                    |  > 40: Vaga livre                                                       |
+| `Luminosidade`     | Nível de luz captado pelo sensor (ex.: ambientes escuros têm valores altos). |
+| `Data`             | Dia em que a vaga foi monitorada (formato `DD/MM/AAAA`).                 |
+| `Início do Período`| Horário em que o status da vaga começou (formato `HH:MM:SS`).            |
+| `Fim do Período`   | Horário em que o status da vaga terminou (formato `HH:MM:SS`).           |
+| `Duração do Período`| Total de tempo em que a vaga ficou no mesmo estado (`hh:mm:ss`).        |
 
-- is_available:
-    - Se o valor de light estiver acima de 600 e da distance estiver abaixo de 40, is_available será 0 (indicando que possui carro na vaga) , caso o valor de is_available seja 1, indica que a vaga esta disponível (ou seja, luminosidade boa,  e distancia maior que 40) **OSB: Para uma vaga ser considerada ocupada ambos os parametros (light e distance) devem atender os requisitos fornecidos**. 
+---
 
-- Dia:
-    - Indica qual dia foi ocupada a vaga no formato DD/MM/AAAA 
+## Exemplo de Dados
 
-- Horário Entrada e Saída:
-    - Indica o horáro de entrada e saída de veiculos de uma vaga. Vagas que não tiverem os horários cadastrados indica que estavam desocupadas. Por exemplo: Vaga A1: d=30, l=800, isava=0 -> nesse caso, vai possuir um horário de entrada (ocupação da vaga por um veículo) e saída. Formato vai ser: HH:MM:SS (Exemplos gerados o SS vai ser sempre 0)
+### Vaga Ocupada (Status = 0)
+| Vaga | Status | Distance | Luminosidade | Data       | Início do Período | Fim do Período | Duração do Período |
+|------|--------|----------|--------------|------------|-------------------|----------------|--------------------|
+| A1   | 0      | 34       | 711          | 16/08/2024 | 06:23:00         | 07:15:05       | 00:52:05          |
 
-- Vaga:
-    - Indica a vaga com seus demais dados. Ex: Vaga A1:
+### Vaga Livre (Status = 1)
+| Vaga | Status | Distance | Luminosidade | Data       | Início do Período | Fim do Período | Duração do Período |
+|------|--------|----------|--------------|------------|-------------------|----------------|--------------------|
+| A1   | 1      | 150      | 556          | 16/08/2024 | 07:15:05         | 08:31:00       | 01:15:55          |
 
+---
 
-Sabendo que o estacionamento ficará aberto de segunda a segunda das 6hrs da manhã até as 22hrs da noite, para randomizarmos os horários geraremos valores floats com 2 digitos entre 6 e 22. Esses valores representam os nossos horários e a % da dos minutos de uma hora, por exemplo:
+## Algoritmo de Geração de Dados Sintéticos
 
-| Valor (float)     | Calc min         | Resultado em horas         |
-|-------------------|------------------|----------------------------|
-| 8.20              | 60(min) * 0,2   | 8hrs e 12min                |
-| 20.50             | 60 * 0,5        | 20 horas 30min              |
-| 5400              | 5400 / 3600     | 1,5 horas (1 hora e 30 min) |
+O repositório contém um algoritmo em Python para gerar dados simulados de ocupação e disponibilidade de vagas. Este algoritmo é essencial para treinar e validar os modelos de IA que realizam as predições e notificações.
 
+### Pontos-Chave do Algoritmo
 
-48 vagas -> A1 -> 21600 -> 22400 -> ficou 12min
-6-22 ->
+1. **Geração de Períodos Ocupados**:
+   - Cria períodos aleatórios em que uma vaga está ocupada, com `distance ≤ 40` e `luminosidade ≥ 600`.
 
-A1 -> is_available = 0 (vaga ocupada)
-    -> distance <= 40 (random int(entre 0-40))
-    -> light >= 600 (random int(entre 600-900))
-    -> Tem um hora_Ini -> hora_fim
+2. **Cálculo de Períodos Livres**:
+   - Identifica automaticamente os períodos entre ocupações em que a vaga está livre, com `distance > 40` e `luminosidade < 600`.
 
+3. **Salvamento Dinâmico**:
+   - Os dados são salvos em um arquivo Excel (`training_data.xlsx`), que pode ser reutilizado em etapas de análise ou treinamento de IA.
 
-| Vaga || is_available | distance | light | | DIA/MES/ANO | | Hora entrada | | Hora Saída | | Tempo Total Ocupado |
+4. **Logs de Execução**:
+   - O algoritmo registra logs detalhados para monitorar seu progresso e identificar possíveis problemas.
 
-1 (A1) vaga -> pode ser ocupada 06:00 -> 06:30 -> 06:40-6:50 -> 7:00 -> 7:45...
-A1 6 - 22
-
-for vaga in range(48):
-    random(float(6-22)) -> 6.50 -> 06:30 -> 60 * 0,5 
-
-Precisa de um range para o numero de dias, exemplo:
-
-dias = 100
-data_ini = 21/10/2023 -> 100 
-
-for dia in range(dias): # seriam gerados dados para 100 dias
-    for vaga in range(48): # Para cada seria gerado os dados para as vagas
-        array = [['hora_ini', 'hora_fim'], ['hora_ini', 'hora_fim'], ['hora_ini', 'hora_fim']]
-
-        for batata in array:
-            random(float(6-22)) randint(hora_Ini)
-            hora_ini = random(batata[0], batata[1])
-            hora_fim = random(hora_Ini, batata[1])
-            adiciona no excel
-
-ficou livre 06:30 -> 06:40
--- src/gen_data.py
+++ src/gen_data.py
@@ -10,110 +10,115 @@ logging.basicConfig(
     format='%(asctime)s - %(levelname)s - %(message)s'
 )
 
-logging.info("Início da execução do script.")
-parking_spots = [f"{letter}{number}" for letter in "ABC" for number in range(1, 17)]
-start_date = '16/08/2024'
+def generate_training_data():
 
-data_columns = {
-    "Vaga": [],
-    "Status": [],
-    "Distância (cm)": [],
-    "Luminosidade": [],
-    "Data": [],
-    "Início do Período": [],
-    "Fim do Período": [],
-    "Duração do Período": []
-}
+    logging.info("Início da execução do script.")
+    parking_spots = [f"{letter}{number}" for letter in "ABC" for number in range(1, 17)]
+    start_date = '16/08/2024'
 
-df = pd.DataFrame(data_columns)
-df.to_excel('./data/training_data.xlsx')
+    data_columns = {
+        "Vaga": [],
+        "Status": [],
+        "Distância (cm)": [],
+        "Luminosidade": [],
+        "Data": [],
+        "Início do Período": [],
+        "Fim do Período": [],
+        "Duração do Período": []
+    }
 
-opening_time = datetime.strptime("06:00:00", "%H:%M:%S")
-closing_time = datetime.strptime("22:00:00", "%H:%M:%S")
-start_date_obj = datetime.strptime(start_date, '%d/%m/%Y')
+    df = pd.DataFrame(data_columns)
+    df.to_excel('./data/training_data.xlsx')
 
-for day_offset in range(100):
-    current_date = start_date_obj + timedelta(days=day_offset)
-    formatted_date = current_date.strftime('%d/%m/%Y')
-    logging.info(f"Processando dados para o dia {formatted_date}.")
+    opening_time = datetime.strptime("06:00:00", "%H:%M:%S")
+    closing_time = datetime.strptime("22:00:00", "%H:%M:%S")
+    start_date_obj = datetime.strptime(start_date, '%d/%m/%Y')
 
-    for spot in parking_spots:
-        logging.info(f"Iniciando processamento da vaga {spot} para o dia {formatted_date}.")
-        n = random.randint(1, 5)
-        last_departure_time = opening_time
-        occupied_periods = []
+    for day_offset in range(100):
+        current_date = start_date_obj + timedelta(days=day_offset)
+        formatted_date = current_date.strftime('%d/%m/%Y')
+        logging.info(f"Processando dados para o dia {formatted_date}.")
 
-        for _ in range(n):
-            distance = random.randint(0, 40)
-            light = random.randint(600, 1000)
-            min_start_time = last_departure_time + timedelta(minutes=random.randint(1, 30))
+        for spot in parking_spots:
+            logging.info(f"Iniciando processamento da vaga {spot} para o dia {formatted_date}.")
+            n = random.randint(1, 5)
+            last_departure_time = opening_time
+            occupied_periods = []
 
-            if min_start_time >= closing_time:
-                break
-            start_time = min_start_time
+            for _ in range(n):
+                distance = random.randint(0, 40)
+                light = random.randint(600, 1000)
+                min_start_time = last_departure_time + timedelta(minutes=random.randint(1, 30))
 
-            hour_interval = random.randint(0, 2)
-            min_interval = random.randint(0, 59)
-            sec_interval = random.randint(0, 59)
+                if min_start_time >= closing_time:
+                    break
+                start_time = min_start_time
 
-            departure_time = start_time + timedelta(
-                hours=hour_interval,
-                minutes=min_interval,
-                seconds=sec_interval
-            )
+                hour_interval = random.randint(0, 2)
+                min_interval = random.randint(0, 59)
+                sec_interval = random.randint(0, 59)
 
-            if departure_time > closing_time:
-                break
-            total_occupation_time = departure_time - start_time
+                departure_time = start_time + timedelta(
+                    hours=hour_interval,
+                    minutes=min_interval,
+                    seconds=sec_interval
+                )
 
-            occupied_periods.append((start_time, departure_time))
+                if departure_time > closing_time:
+                    break
+                total_occupation_time = departure_time - start_time
 
-            row_data = {
-                "Vaga": spot,
-                "Status": '0',
-                "Distância (cm)": distance,
-                "Luminosidade": light,
-                "Data": formatted_date,
-                "Início do Período": start_time.strftime("%H:%M:%S"),
-                "Fim do Período": departure_time.strftime("%H:%M:%S"),
-                "Duração do Período": str(total_occupation_time)
-            }
+                occupied_periods.append((start_time, departure_time))
 
-            df.loc[len(df)] = row_data
-            logging.info(f"Período ocupado gerado: {row_data}")
-            df.to_excel('./data/training_data.xlsx', index=False)
-            last_departure_time = departure_time
+                row_data = {
+                    "Vaga": spot,
+                    "Status": '0',
+                    "Distância (cm)": distance,
+                    "Luminosidade": light,
+                    "Data": formatted_date,
+                    "Início do Período": start_time.strftime("%H:%M:%S"),
+                    "Fim do Período": departure_time.strftime("%H:%M:%S"),
+                    "Duração do Período": str(total_occupation_time)
+                }
 
-        free_periods = []
-        previous_end = opening_time
+                df.loc[len(df)] = row_data
+                logging.info(f"Período ocupado gerado: {row_data}")
+                df.to_excel('./data/training_data.xlsx', index=False)
+                last_departure_time = departure_time
 
-        for start, end in occupied_periods:
-            if previous_end < start:
-                free_periods.append((previous_end, start))
-            previous_end = end
-        if previous_end < closing_time:
-            free_periods.append((previous_end, closing_time))
+            free_periods = []
+            previous_end = opening_time
 
-        for free_start, free_end in free_periods:
-            if free_start >= free_end:
-                continue
+            for start, end in occupied_periods:
+                if previous_end < start:
+                    free_periods.append((previous_end, start))
+                previous_end = end
+            if previous_end < closing_time:
+                free_periods.append((previous_end, closing_time))
 
-            distance_free = random.randint(40, 400)
-            light_free = random.randint(0, 600)
+            for free_start, free_end in free_periods:
+                if free_start >= free_end:
+                    continue
 
-            row_free = {
-                "Vaga": spot,
-                "Status": '1',
-                "Distância (cm)": distance_free,
-                "Luminosidade": light_free,
-                "Data": formatted_date,
-                "Início do Período": free_start.strftime("%H:%M:%S"),
-                "Fim do Período": free_end.strftime("%H:%M:%S"),
-                "Duração do Período": str(free_end - free_start)
-            }
+                distance_free = random.randint(40, 400)
+                light_free = random.randint(0, 600)
 
-            df.loc[len(df)] = row_free
-            logging.info(f"Período livre gerado: {row_free}")
-            df.to_excel('./data/training_data.xlsx', index=False)
+                row_free = {
+                    "Vaga": spot,
+                    "Status": '1',
+                    "Distância (cm)": distance_free,
+                    "Luminosidade": light_free,
+                    "Data": formatted_date,
+                    "Início do Período": free_start.strftime("%H:%M:%S"),
+                    "Fim do Período": free_end.strftime("%H:%M:%S"),
+                    "Duração do Período": str(free_end - free_start)
+                }
 
-logging.info("Dados de Treinamento gerados com sucesso.")
+                df.loc[len(df)] = row_free
+                logging.info(f"Período livre gerado: {row_free}")
+                df.to_excel('./data/training_data.xlsx', index=False)
+
+    logging.info("Dados de Treinamento gerados com sucesso.")
+    
+if __name__ == "__main__":
+    generate_training_data()
